35d6c1e0c1a605b20d1904d8154cea2d6fbdb98a
Israil Alam <52991105+Mdizrail92@users.noreply.github.com>
2021-09-20 11:04:01 +0530

    v0.4(comment section added)

--- index.php
+++ index.php
@@ -6,6 +6,11 @@ include("functions.php");
 
 $user_data = check_login($conn);
 
+date_default_timezone_set('Asia/Kolkata');
+include('comments.php');
+
+
+
 ?>
 
 
@@ -16,15 +21,35 @@ $user_data = check_login($conn);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>Document</title>
 </head>
 
 <body>
+    <div class="container">
+        <a href="logout.php">Logout</a>
+        <h1>Hello World From Israel and Harshavardhan Reddy</h1>
+        <br>
+        <h3>This is <?php echo $user_data['username']; ?> </h3>
 
-    <a href="logout.php">Logout</a>
-    <h1>Hello World From Israel and Harshavardhan Reddy</h1>
-    <br>
-    <h3>This is <?php echo $user_data['username']; ?> </h3>
+
+        <div class="spacer" style="height: 100px;"></div>
+        <!-- comment section -->
+        <img src="images/gal-gadotas (1).jpg" alt="">
+        <div class="spacer" style="height: 30px;"></div>
+        <?php
+        echo "<form method='POST' action='" . setComments($conn) . "'>
+        <input type='hidden' name='uid' value='" . $user_data['username'] . "'>
+        <input type='hidden' name='date' value='" . date('Y-m-d H:i:s') . "'>
+        <textarea name='message'></textarea>    <br>  
+        <button type='submit' name='commentSubmit'>Comment</button>
+    </form>";
+
+        getComments($conn);
+        ?>
+        <!-- comment section ends -->
+
+    </div>
 </body>
 
 </html>
